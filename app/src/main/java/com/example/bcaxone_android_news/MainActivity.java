012f9750ca844a2db87bd1e6055205f022c96850
Dennis Setiawan <dennis_setiawan@bca.co.id>
2022-04-14 22:30:52 +0700

    initial commit

---
+++ app/src/main/java/com/example/bcaxone_android_news/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bcaxone_android_news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
